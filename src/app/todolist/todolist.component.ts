b86fc2b6c43fd43ccd7d7edc6163852888712bbd
gokul403 <gokulkm9@gmail.com>
2021-02-22 14:09:17 +0530

    [todolist]

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { resetFakeAsyncZone } from '@angular/core/testing';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
   selector: 'app-todolist',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todolist.component.css']
 })
 export class TodolistComponent implements OnInit {
+  value1:string=""
+  array:any[]=[]
+  array2:any[]=[]
+  index:any
+ 
+ 
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  setNumber(event:any){
+    this.value1=(event.target as HTMLInputElement).value
+  }
+  array1(){
+    this.array.push(this.value1)
+    console.log(this.array)
+    
+  }
+  remove(a:any){
+    this.index=this.array.indexOf(a)
+    this.array.splice(this.index,1)
+    console.log(this.array)
+    
+  }
+  move(a:any){
+    this.array2.push(a)
+    this.remove(a)
+  
+  }
+  remove2(a:any){
+    this.index=this.array2.indexOf(a)
+    this.array2.splice(this.index,1)
+    console.log(this.array2)
   }
 
 }
